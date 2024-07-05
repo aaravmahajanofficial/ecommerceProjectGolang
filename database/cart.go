cd5111e8455ef904fbe4dbe45860cbdc281c70f8
Aarav Mahajan <aaravmahajan2003@gmail.com>
2024-07-05 05:51:49 +0530

    Update: database

--- database/cart.go
+++ database/cart.go
@@ -1,1 +1,29 @@
 package database
+
+import "errors"
+
+var (
+	ErrCantFindProduct    = errors.New("unable to find the specified product")
+	ErrCantDecodeProducts = errors.New("unable to decode product information")
+	ErrUserIDIsNotValid   = errors.New("user ID is not valid")
+	ErrCantUpdateUser     = errors.New("unable to update user information")
+	ErrCantRemoveItem     = errors.New("unable to remove item from cart")
+	ErrCantGetItem        = errors.New("unable to retrieve item from cart")
+	ErrCantBuyCartItem    = errors.New("unable to process the purchase of cart item")
+)
+
+func AddProductToCart() {
+
+}
+
+func RemoveCartItem() {
+
+}
+
+func BuyItemFromCart() {
+
+}
+
+func InstantBuy() {
+
+}
--- database/databaseSetup.go
+++ database/databaseSetup.go
@@ -1,1 +1,46 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBSet() *mongo.Client {
+
+	context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(context, options.Client().ApplyURI(""))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context, nil)
+
+	if err != nil {
+		log.Println("Connection failed to MongoDB")
+		return nil
+
+	}
+
+	fmt.Println("Connection successfully to MongoDB")
+
+	return client
+
+}
+
+var Client *mongo.Client = DBSet()
+
+func UserData() {
+
+}
+
+func ProductData() {
+
+}

27eb3408b256882a9d491791feac5a25eb77292c
Aarav Mahajan <aaravmahajan2003@gmail.com>
2024-07-08 02:43:29 +0530

    Implement Verify Tokne

--- tokens/tokensGen.go
+++ tokens/tokensGen.go
@@ -1,13 +1,19 @@
 package tokens
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/aaravmahajanofficial/ecommerce-project/database"
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var UserCollection *mongo.Collection = database.UserData(database.Client, "Users")
@@ -54,5 +60,52 @@ func TokenGenerator(email string, firstName string, lastName string, uid string)
 	}
 
 	return token, refreshToken, err
+
+}
+
+func VerifyToken(signedToken string) (claims *SignedDetails, msg string) {
+
+	token, err := jwt.ParseWithClaims(signedToken, &SignedDetails{}, func(t *jwt.Token) (interface{}, error) {
+
+		return []byte(SECRET_KEY), nil
+
+	})
+
+	if err != nil {
+		msg = err.Error()
+		return
+	}
+
+	claims, ok := token.Claims.(*SignedDetails)
+
+	if !ok {
+		msg = "The Token is not valid."
+		return
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		msg = "Toke is expired"
+		return
+	}
+
+	return claims, msg
+
+}
+
+func UpdateAllTokens(signedToken string, signedRefreshToken string, userID string) {
+
+	context, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	filter := bson.D{primitive.E{Key: "_id", Value: userID}}
+	updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	update := bson.D{{Key: "$set", Value: bson.D{primitive.E{Key: "token", Value: signedToken}, primitive.E{Key: "refresh_token", Value: signedRefreshToken}, primitive.E{Key: "updatedat", Value: updated_at}}}}
+
+	_, err := UserCollection.UpdateOne(context, filter, update, options.Update().SetUpsert(true))
+
+	if err != nil {
+		log.Panic(err)
+		return
+	}
 
 }

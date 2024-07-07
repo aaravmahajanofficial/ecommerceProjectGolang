7b0c3605b592ea9d32d6c8a4d2605f3b8c88f437
Aarav Mahajan <aaravmahajan2003@gmail.com>
2024-07-08 04:26:51 +0530

    Implement middleware RemoveCartItem

--- database/cart.go
+++ database/cart.go
@@ -1,6 +1,15 @@
 package database
 
-import "errors"
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/aaravmahajanofficial/ecommerce-project/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 var (
 	ErrCantFindProduct    = errors.New("unable to find the specified product")
@@ -12,11 +21,64 @@ var (
 	ErrCantBuyCartItem    = errors.New("unable to process the purchase of cart item")
 )
 
-func AddProductToCart() {
+func AddProductToCart(context context.Context, productsCollection *mongo.Collection, usersCollection *mongo.Collection, productID primitive.ObjectID, userID string) error {
+
+	filter := bson.D{primitive.E{Key: "_id", Value: userID}}
+	cursor, err := productsCollection.Find(context, filter)
+
+	if err != nil {
+		log.Println(err)
+		return ErrCantFindProduct
+	}
+
+	var itemsToBeAdded []models.ProductUser
+
+	if err = cursor.All(context, &itemsToBeAdded); err != nil {
+		log.Println(err)
+		return ErrCantDecodeProducts
+	}
+
+	objectID, err := primitive.ObjectIDFromHex(userID)
+
+	if err != nil {
+		log.Println(err)
+		return ErrUserIDIsNotValid
+	}
+
+	// need to find the document of the user, to insert this item in the user cart
+	filter = bson.D{primitive.E{Key: "_id", Value: objectID}}
+	update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "usercart", Value: bson.D{primitive.E{Key: "$each", Value: itemsToBeAdded}}}}}}
+	_, err = usersCollection.UpdateOne(context, filter, update)
+
+	if err != nil {
+		log.Println(err)
+		return ErrCantUpdateUser
+	}
+
+	return nil
 
 }
 
-func RemoveCartItem() {
+func RemoveCartItem(context context.Context, productsCollection *mongo.Collection, usersCollection *mongo.Collection, productID primitive.ObjectID, userID string) error {
+
+	// first convert the userID to primitive.ObjectID
+	objectID, err := primitive.ObjectIDFromHex(userID)
+
+	if err != nil {
+		log.Println(err)
+		return ErrUserIDIsNotValid
+	}
+
+	filter := bson.D{primitive.E{Key: "_id", Value: objectID}}
+	update := bson.D{{Key: "$pull", Value: bson.D{primitive.E{Key: "usercart", Value: productID}}}}
+	_, err = usersCollection.UpdateMany(context, filter, update)
+
+	if err != nil {
+		log.Println(err)
+		return ErrCantUpdateUser
+	}
+
+	return nil
 
 }
 
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,38 @@
 package middleware
+
+import (
+	"errors"
+	"net/http"
+
+	token "github.com/aaravmahajanofficial/ecommerce-project/tokens"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Authorization() gin.HandlerFunc {
+
+	return func(ctx *gin.Context) {
+
+		ClientToken := ctx.Request.Header.Get("Token")
+
+		if ClientToken == "" {
+			ctx.AbortWithError(http.StatusInternalServerError, errors.New("No authorization header provided"))
+			return
+		}
+
+		claims, err := token.VerifyToken(ClientToken)
+
+		if err != "" {
+
+			ctx.AbortWithError(http.StatusInternalServerError, errors.New("Token validation failed"))
+			return
+
+		}
+
+		ctx.Set("Email", claims.Email)
+		ctx.Set("UID", claims.UID)
+		ctx.Next()
+
+	}
+
+}
